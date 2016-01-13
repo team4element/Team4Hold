8a8ac23d79743217404a3b0ed8a13d738af9489b
Justin Punzalan <justpunz@Live.com>
2016-01-12 16:53:35 -0800

    Added Tank Drive Command

--- src/org/usfirst/frc/team4/robot/commands/TankDrive.java
+++ src/org/usfirst/frc/team4/robot/commands/TankDrive.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team4.robot.commands;
 
 import org.usfirst.frc.team4.robot.Robot;
+import org.usfirst.frc.team4.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.command.Command;
 
@@ -15,11 +16,12 @@ public class TankDrive extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	System.out.println("Tank Drive Initialized");
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	
+    	Robot.chassis.tankDrive(RobotMap.driveCont, RobotMap.driveCont);
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -29,10 +31,12 @@ public class TankDrive extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.chassis.stop();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	end();
     }
 }
