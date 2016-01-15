bfca40854025092b89bb793568b36c4388e995e8
Justin Punzalan <justpunz@Live.com>
2016-01-14 17:55:48 -0800

    Added Rumble

--- src/org/usfirst/frc/team4/robot/commands/GearToggle.java
+++ src/org/usfirst/frc/team4/robot/commands/GearToggle.java
@@ -4,42 +4,68 @@ import org.usfirst.frc.team4.robot.Robot;
 import org.usfirst.frc.team4.robot.RobotMap;
 import org.usfirst.frc.team4.robot.subsystems.Chassis.DriveSpeed;
 
+import edu.wpi.first.wpilibj.Joystick.RumbleType;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
  */
 public class GearToggle extends Command {
+
+	public GearToggle() {
+	}
+
+	// Called just before this Command runs the first time
+	protected void initialize() {
+		if (Robot.oi.R1(RobotMap.driveCont)) {
+			
+			if (Robot.chassis.currentGear != DriveSpeed.HIGH){
+				RobotMap.driveCont.setRumble(RumbleType.kLeftRumble, .75f);
+				RobotMap.driveCont.setRumble(RumbleType.kRightRumble, .75f);
 	
-    public GearToggle() {
-    }
+				Timer.delay(.15);
+	
+				RobotMap.driveCont.setRumble(RumbleType.kLeftRumble, 0);
+				RobotMap.driveCont.setRumble(RumbleType.kRightRumble, 0);
+			}
+			
+			Robot.chassis.currentGear = DriveSpeed.HIGH;
+			
+		} else if (Robot.oi.L1(RobotMap.driveCont)) {
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    	if (Robot.oi.R1(RobotMap.driveCont)) {
-    		Robot.chassis.currentGear = DriveSpeed.HIGH;
-    	} else if (Robot.oi.L1(RobotMap.driveCont)){
-    		Robot.chassis.currentGear = DriveSpeed.LOW;
-    	} else {
-    		System.out.println("No Button Mapped");
-    	}
-    }
+			if (Robot.chassis.currentGear != DriveSpeed.LOW){
+				RobotMap.driveCont.setRumble(RumbleType.kLeftRumble, .75f);
+				RobotMap.driveCont.setRumble(RumbleType.kRightRumble, .75f);
+	
+				Timer.delay(.15);
+	
+				RobotMap.driveCont.setRumble(RumbleType.kLeftRumble, 0);
+				RobotMap.driveCont.setRumble(RumbleType.kRightRumble, 0);
+			}
+			
+			Robot.chassis.currentGear = DriveSpeed.LOW;
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    }
+		} else {
+			System.out.println("No Button Mapped");
+		}
+	}
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return true;
-    }
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+	}
 
-    // Called once after isFinished returns true
-    protected void end() {
-    }
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return true;
+	}
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    }
+	// Called once after isFinished returns true
+	protected void end() {
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+	}
 }
