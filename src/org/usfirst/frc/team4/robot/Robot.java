0791884c0b00244a619c6b2115fc4cdc3da9e333
justadev <justpunz@live.com>
2016-02-21 13:30:23 -0800

    Added teleop log

--- src/org/usfirst/frc/team4/robot/Robot.java
+++ src/org/usfirst/frc/team4/robot/Robot.java
@@ -96,6 +96,7 @@ public class Robot extends IterativeRobot {
         // continue until interrupted by another command, remove
         // this line or comment it out.
         if (autonomousCommand != null) autonomousCommand.cancel();
+        log();
     }
 
     /**
